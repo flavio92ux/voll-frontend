9f0a094a2126d1e588cfc401ff4ebe7814a19cfb
Flavio Franco <flavio.win@gmail.com>
2022-01-31 07:57:03 -0300

    Configurando o socket

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script>
+import SocketioService from './services/socketio.service.js';
+
+export default {
+  name: 'App',
+  components: {
+  },
+  created() {
+    SocketioService.setupSocketConnection();
+  },
+  beforeUnmount() {
+    SocketioService.disconnect();
+  }
+}
+</script>
